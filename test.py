f708cb8246d961e25492b0ba81f584166062beab
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-11-11 14:29:53 +0100

    Tests that don't depend on each other

--- test.py
+++ test.py
@@ -9,37 +9,83 @@ try:
 except KeyError:
     exit(1)
 
-tarallo.tsession = Tarallo(t_url, t_user, t_pass)
-def test_create_session():
-    assert tarallo.tsession
 
-def test_equal_tsession():
+def test_create_session_and_tsession_var():
+    tarallo.tsession = Tarallo(t_url, t_user, t_pass)
+    assert tarallo.tsession is not None
     assert tarallo.tsession == tarallo.get_tsession()
+    tarallo.tsession = None
 
-status = tarallo.tsession.status()
+
 def test_status_before_login():
-    assert status == False
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    status = tarallo_session.status()
+    assert status is False
 
-def test_logout_before_login():
-    assert tarallo.tsession.logout() == False
-
-login = tarallo.tsession.login()
-def test_tarallo_request():
-    assert tarallo.tsession.request
-
-def test_tarallo_cookie():
-    assert tarallo.tsession.cookie
-
-def test_tarallo_login():
-    assert tarallo.tsession.request.status_code == 204
-    assert login == True
 
 def test_status_after_login():
-    assert tarallo.tsession.status() == True
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    tarallo_session.login()
+    status = tarallo_session.status()
+    assert status is True
 
-def test_tarallo_logout():
-    assert tarallo.tsession.logout() == True
 
-def test_tarallo_cookie_after_logout():
-    assert tarallo.tsession.cookie == None
+def test_logout_before_login():
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    tarallo_session.logout()
+    status = tarallo_session.status()
+    assert status is False
 
+
+def test_login():
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    result = tarallo_session.login()
+    assert result is True
+
+
+def test_invalid_login():
+    tarallo_session = Tarallo(t_url, 'invalid', 'invalid')
+    result = tarallo_session.login()
+    assert result is False
+    status = tarallo_session.status()
+    assert status is False
+
+
+def test_tarallo_login_and_logut():
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    assert tarallo_session.status() is False
+    assert tarallo_session.login() is True
+    assert tarallo_session.status() is True
+    assert tarallo_session.logout() is True
+    assert tarallo_session.status() is False
+
+
+def test_tarallo_request_after_status():
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    assert tarallo_session.request is None
+    tarallo_session.login()
+    tarallo_session.status()
+    assert tarallo_session.request is not None
+
+
+def test_tarallo_request_after_login():
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    assert tarallo_session.request is None
+    tarallo_session.login()
+    assert tarallo_session.request is not None
+
+
+def test_tarallo_request_after_invalid_login():
+    tarallo_session = Tarallo(t_url, 'invalid', 'invalid')
+    assert tarallo_session.request is None
+    tarallo_session.login()
+    assert tarallo_session.request is not None
+
+
+def test_tarallo_cookie():
+    tarallo_session = Tarallo(t_url, t_user, t_pass)
+    assert tarallo_session.cookie is None
+    tarallo_session.login()
+    assert tarallo_session.cookie is not None
+    tarallo_session.logout()
+    assert tarallo_session.cookie is None
